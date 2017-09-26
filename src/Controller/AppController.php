473598fa40feb1ad14d9694134eb61146b90dea4
Erica Dee Fox <ericadeefox@gmail.com>
2017-09-26 11:22:03 -0400

    Upgrade CKEditor to 4.7.3. Solve #14

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -31,7 +31,7 @@ class AppController extends Controller
 {
     public $helpers = [
         'AkkaCKEditor.CKEditor' => [
-            'version' => '4.4.7', // Default Option
+            'version' => '4.7.3', // Default Option
             'distribution' => 'basic'
             // Default Option / Other options => 'basic', 'standard', 'standard-all', 'full-all'
         ],
